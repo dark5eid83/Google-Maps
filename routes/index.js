71b20d0378c071b2381dc6cd4345f96e045a1a9d
cbartram <cbartram3@gmail.com>
2018-04-21 19:56:18 -0400

    Cleaned up unneccesary files, added chalk module

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+
+/**
+ * Handles showing the Home page/index route
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', key: process.env.GOOGLE_API_KEY, location: 'Seattle+Washington' });
+  //Default location is set to Seattle Washington
+  res.render('index', { key: process.env.GOOGLE_API_KEY, location: 'Seattle+Washington' });
 });
 
+
+/**
+ * Handles the form being submitted.
+ */
 router.post('/place', (req, res) => {
-    //The result of the form being submitted
-    res.render('index', { title: 'Express', key: process.env.GOOGLE_API_KEY, location: req.body.location.trim() })
+    //The result of the form being submitted re-render the home page with an updated location
+    res.render('index', { key: process.env.GOOGLE_API_KEY, location: req.body.location.trim() })
 });
 
 module.exports = router;
