c260157472fd6b09f820069c56ebd772c54e8125
cbartram <cbartram3@gmail.com>
2018-04-21 21:14:53 -0400

    Added frontend styles refactored routes

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const { searches, users } = require('../models');
 /**
  * Handles showing the Home page/index route
  */
-router.get('/', function(req, res, next) {
-  //Default location is set to Seattle Washington
-  res.render('index', {
-      key: process.env.GOOGLE_API_KEY,
-      location: 'Seattle+Washington',
-      searches: []
-  });
+router.get('/', (req, res) => {
+     //Default location is set to Seattle Washington
+     searches.all().then(result => {
+         res.render('index', {
+             key: process.env.GOOGLE_API_KEY,
+             location: req.query.location,
+             searches: result
+         });
+     });
 });
 
 
@@ -23,14 +25,7 @@ router.post('/place', (req, res) => {
      searches.create({
          name: req.body.location.trim()
      }).then(() => {
-         searches.all().then(result => {
-             //The result of the form being submitted re-render the home page with an updated location
-             res.render('index', {
-                 key: process.env.GOOGLE_API_KEY,
-                 location: req.body.location.trim(),
-                 searches: result
-             })
-         })
+         res.redirect(`/?location=${req.body.location.trim()}`)
      });
 });
 
