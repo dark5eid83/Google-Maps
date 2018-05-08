b64bc3f149bf65a744a9afee2454963737a7fe96
cbartram <cbartram3@gmail.com>
2018-05-08 12:26:11 -0400

    Fixed logo link

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
                     <footer className="pt-4 my-md-5 pt-md-5 border-top">
                         <div className="row">
                             <div className="col-12 col-md">
-                                <img className="mb-2" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24" />
+                                <img className="mb-2" src="/images/logo.png" alt="" width="24" height="24" />
                                     <small className="d-block mb-3 text-muted">&copy; 2017-2018</small>
                             </div>
                             <div className="col-6 col-md">
